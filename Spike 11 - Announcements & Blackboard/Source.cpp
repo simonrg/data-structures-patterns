468f373a4b1c34283f833c6c003933961387cbcb
Simon <6960464@student.swin.edu.au>
2015-09-15 11:58:17 +1000

    Rewrote Blackboard code.

--- Spike 11 - Announcements & Blackboard/Source.cpp
+++ Spike 11 - Announcements & Blackboard/Source.cpp
@@ -6,155 +6,114 @@ using namespace std;
 
 
 
+class Subscriber{
+protected:
+	string datafield;
+public:
+	virtual void OutputResult() = 0;
+	void receiveMessage(string message){ datafield = message; }
+};
 template <class T> class Message{
 private:
 	T message;
-	vector<int> subscribers;
 public:
-	void packMessage(T);
-	T unpackMessage(int);
-	int registerSubscriber(int);
+	void packMessage(T gv){ message = gv };
+	T unpackMessage(){ return message; }
 };
-template<class T> void Message<T>::packMessage(T gv){
-	message = gv;
-}
-template<class T> T Message<T>::unpackMessage(int sub_id){
-	//search subscribers for id
-	for (int i : subscribers){
-		if (i == sub_id)
-			//object has registered id
-			return message;
-	}
-	//object doesn't have registered id
-	return "Not a registered subscriber";
-}
-template<class T> int Message<T>::registerSubscriber(int subscriber){
-	//generate a subscriber id and store it
-	subscriber = rand();
-	subscribers.push_back(subscriber);
-
-	return subscriber;
-}
 
 
-//blackboard
-template <class T> class StoreMessages{
+
+
+
+
+
+template <class T> class Blackboard{
 private:
 	vector<Message<T>> messages;
+	Message<T> top_message;
+	vector<Subscriber*> subscribers;
 public:
 	void saveMessage(Message<T> m){ messages.push_back(m); }
+	void sendSubscriber(Subscriber*);
+	void sendAll();
+	void registerSubscriber(Subscriber*);
+	void removeSubscriber(Subscriber*);
 };
+template<class T> void Blackboard<T>::sendSubscriber(Subscriber* s){
+	top_message = messages.front();
+
+	for (Subscriber* registered_sub : subscribers){
+		if (s == registered_sub)
+			s->receiveMessage(top_message.unpackMessage());
+	}
+	s->OutputResult();
+}
+template<class T> void Blackboard<T>::sendAll(){
+	top_message = messages.front();
+
+	for (Subscriber* registered_sub : subscribers){
+		registered_sub->receiveMessage(top_message.unpackMessage());
+		registered_sub->OutputResult();
+	}
+}
+template<class T> void Blackboard<T>::registerSubscriber(Subscriber* s){
+	subscribers.push_back(s);
+}
+template<class T> void Blackboard<T>::removeSubscriber(Subscriber* s){
+	subscribers.erase(s);
+}
 
 
 
-//sender
-template <class T> class Sender{
-private:
-	T datafield;
+
+class Component1 : public Subscriber{
 public:
-	T sendStuff();
+	void OutputResult(){ cout << "Component 1 messages: " << datafield << endl; }
 };
-template<class T> T Sender<T>::sendStuff(){
-	datafield = "A message to all subscribed objects";
-	return datafield;
-}
 
-
-
-//registered
-template <class T> class Component1{
-private:
-	T datafield;
-	int subscriber_no;
+class Component2 : public Subscriber{
 public:
-	void OutputResult();
-	void receiveMessage(Message<T>, int);
-	int getSubscriber(){ return subscriber_no; }
-	void setSubscriber(int s){ subscriber_no = s; }
+	void OutputResult() { cout << "Component 2 messages: " << datafield << endl; }
 };
-template<class T> void Component1<T>::OutputResult(){
-	cout << "Component 1 received a message: " << datafield << endl;
-}
-template<class T> void Component1<T>::receiveMessage(Message<T> gm, int sub_id){
-	datafield = gm.unpackMessage(sub_id);
-}
 
-template <class T> class Component3{
-private:
-	T datafield;
-	int subscriber_no;
+class Component3 : public Subscriber{
 public:
-	void OutputResult();
-	void receiveMessage(Message<T>, int);
-	int getSubscriber(){ return subscriber_no; }
-	void setSubscriber(int s){ subscriber_no = s; }
+	void OutputResult(){ cout << "Component 3 messages: " << datafield << endl; }
 };
-template<class T> void Component3<T>::OutputResult(){
-	cout << "Component 3 received a message: " << datafield << endl;
-}
-template<class T> void Component3<T>::receiveMessage(Message<T> gm, int sub_id){
-	datafield = gm.unpackMessage(sub_id);
-}
 
 
-
-//not registered
-template <class T> class Component2{
-private:
-	T datafield;
-	int subscriber_no;
-public:
-	void OutputResult();
-	void receiveMessage(Message<T>, int);
-	int getSubscriber(){ return subscriber_no; }
-	void setSubscriber(int s){ subscriber_no = s; }
-};
-template<class T> void Component2<T>::OutputResult(){
-	cout << "Component 2 received a message: " << datafield << endl;
-}
-template<class T> void Component2<T>::receiveMessage(Message<T> gm, int sub_id){
-	datafield = gm.unpackMessage(sub_id);
-}
 
 
 int main(){
-	StoreMessages<string> b;
+	Blackboard<string> b;
 
-	Sender<string> s;
-	string message = s.sendStuff();
-	//string message = "default message";
-
-	//example objects
-	Component1<string> c1;
-	Component2<string> c2;
-	Component3<string> c3;
-
-	//the object to transport the data
+	//message object
 	Message<string> m;
+	m.packMessage("A message to my subscribers!");
+
+	//save message to blackboard
+	b.saveMessage(m);
+
+
+	//some subscribers
+	Subscriber *sub1 = new Component1();
+	Subscriber *sub2 = new Component2();
+	Subscriber *sub3 = new Component3();
 
 	//register subscribers
-	//only registered subscribers will receive the message
-	int sub_no1 = m.registerSubscriber(c1.getSubscriber());
-	c1.setSubscriber(sub_no1);
-	int sub_no2 = m.registerSubscriber(c3.getSubscriber());
-	c3.setSubscriber(sub_no2);
+	//so they can receive messages
+	b.registerSubscriber(sub1);
+	b.registerSubscriber(sub2);
+	b.registerSubscriber(sub3);
 
-	//pack the message
-	m.packMessage(message);
 
-	//receive a message
-	//pass the message data, the subscriber id
-	c1.receiveMessage(m, c1.getSubscriber());
-	c2.receiveMessage(m, c2.getSubscriber());
-	c3.receiveMessage(m, c3.getSubscriber());
+	//send a message to a specific subscriber
+	cout << "Send to a specific subscriber" << endl;
+	b.sendSubscriber(sub2);
 
-	//verify only subscribers receive the message
-	c1.OutputResult();
-	c2.OutputResult();	//not subscribed
-	c3.OutputResult();
-
-	//keep the message
-	b.saveMessage(m);
+	//send a message to all subscribers
+	cout << endl << "Send to all subscribers" << endl;
+	b.sendAll();
 
 	system("pause");
 	return 0;
